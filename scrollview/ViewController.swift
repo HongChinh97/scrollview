3bd1ba31cb73cd714cfbddbe5c5ec16be04f1172
admin <chiinhuv@gmail.com>
2018-07-24 08:23:45 +0700

    add

--- scrollview/ViewController.swift
+++ scrollview/ViewController.swift
@@ -8,16 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIScrollViewDelegate {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        scrollView.minimumZoomScale = 5.0
+        scrollView.maximumZoomScale = 10.0
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return(imageView)
     }
 
 
